a75407b2cb08fbba6245163d93f040f4fceb6207
Wahid Hoque <wahidhoquee@gmail.com>
2020-10-18 14:52:41 +0600

    refactor > Code intendetion

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { configureStore } from '@reduxjs/toolkit'
+import React from "react";
+import ReactDOM from "react-dom";
+import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
-import { Provider } from "react-redux"
+import { Provider } from "react-redux";
 
-import App from './app'
+import App from "./app";
 
-const rootReducer = combineReducers({ })
+const rootReducer = combineReducers({});
 
 const store = configureStore({
-    reducer: rootReducer, 
-  });
+  reducer: rootReducer,
+});
 
-const app = (  
-        <Provider store = {store}>
-            <App />
-        </Provider>
-    )
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
-ReactDOM.render( app ,document.getElementById('root'));
+ReactDOM.render(app, document.getElementById("root"));
