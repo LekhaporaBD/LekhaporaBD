b8ace6045e882d3a5c9963ab6d40346875431313
Wahid Hoque <wahidhoquee@gmail.com>
2020-10-22 22:28:46 +0600

    added > hero section

--- src/components/Dashboard/Hero.jsx
+++ src/components/Dashboard/Hero.jsx
@@ -1,25 +1,29 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 
-import styles from './Hero.module.scss'
-import cover from '../../assets/cover.svg';
-
+import styles from "./Hero.module.scss";
+import cover from "../../assets/cover.svg";
 
 const Hero = () => {
   return (
-      <div className={styles.hero}>
-        <div>
-          <h1 className={styles.title}>Welcome Back, <strong>Wahid!</strong></h1>
+    <div className={styles.hero}>
+      <div className={styles.heroWrapper}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>
+            Welcome Back, <strong>Nazrul!</strong>
+          </h1>
           <p className={styles.subtitle}>
-            You have completed 76% of the lessons.<br/>
+            You have completed 76% of the lessons.
+            <br />
             Progress is very good.
           </p>
           <button className={styles.button}>View Progress</button>
         </div>
         <div>
-          <img src={cover} alt="Cover hero image"  className={styles.image}/>
+          <img src={cover} alt="" className={styles.image} />
         </div>
       </div>
+    </div>
   );
 };
 
