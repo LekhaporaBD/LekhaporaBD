73761a9d465ee93ed26428f0b80156ed11a19631
Wahid Hoque <wahidhoquee@gmail.com>
2020-10-23 02:08:17 +0600

    added > summaries section

--- src/pages/Main_Menu/Dashboard/index.jsx
+++ src/pages/Main_Menu/Dashboard/index.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import Hero from '../../../components/Dashboard/Hero'
+import React from "react";
+import Hero from "../../../components/Dashboard/Hero";
+import Summaries from "../../../components/Dashboard/Summaries";
 
 const Dashboard = () => {
-    return (
-        <Hero/>
-    )
-}
+  return (
+    <>
+      <Hero />
+      <Summaries />
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
