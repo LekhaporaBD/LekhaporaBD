3e6ebd47a86abc36992e83a73ef3e74dcc7cde53
Wahid Hoque <wahidhoquee@gmail.com>
2020-10-22 22:56:39 +0600

    styled > layout's main body padding

--- src/components/Dashboard/Hero.jsx
+++ src/components/Dashboard/Hero.jsx
@@ -7,21 +7,19 @@ import cover from "../../assets/cover.svg";
 const Hero = () => {
   return (
     <div className={styles.hero}>
-      <div className={styles.heroWrapper}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>
-            Welcome Back, <strong>Nazrul!</strong>
-          </h1>
-          <p className={styles.subtitle}>
-            You have completed 76% of the lessons.
-            <br />
-            Progress is very good.
-          </p>
-          <button className={styles.button}>View Progress</button>
-        </div>
-        <div>
-          <img src={cover} alt="" className={styles.image} />
-        </div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>
+          Welcome Back, <strong>Nazrul!</strong>
+        </h1>
+        <p className={styles.subtitle}>
+          You have completed 76% of the lessons.
+          <br />
+          Progress is very good.
+        </p>
+        <button className={styles.button}>View Progress</button>
+      </div>
+      <div>
+        <img src={cover} alt="" className={styles.image} />
       </div>
     </div>
   );
